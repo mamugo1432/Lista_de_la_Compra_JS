64d3f268222716b86744fd8a306cd8169b2fae90
mamugo1432 <mamugo1432@gmail.com>
2025-04-30 20:04:15 +0200

    Añadiendo los eventsListener del javascript

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+document.getElementById("botonAñadir").addEventListener("click", añadirProducto);
+document.getElementById("botonEliminar").addEventListener("click", eliminarProducto);
+document.getElementById("botonEliminarPos").addEventListener("click", eliminarProductoPorPosicion);
+
+});
+
